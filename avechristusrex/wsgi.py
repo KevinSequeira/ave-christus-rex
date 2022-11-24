67bcc219c73d09ec803759d39985d5d3f2269754
KevinSequeira <iamkevinsequeira@gmail.com>
2022-11-24 18:11:04 +1030

    Latest changes

--- avechristusrex/wsgi.py
+++ avechristusrex/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deigloriam.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'avechristusrex.settings')
 
 application = get_wsgi_application()
