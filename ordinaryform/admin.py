43f866316bfd96d8bc17fc8c3de87250fa051209
KevinSequeira <iamkevinsequeira@gmail.com>
2023-09-05 01:42:06 +0930

    Added TinyMCE

--- ordinaryform/admin.py
+++ ordinaryform/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from ordinaryform.models import OrdinaryTimePrayer, OrdinaryTimeReading, AdventPrayer, AdventReading
 
 # Register your models here.
+
+class ordinaryTimePrayer(admin.ModelAdmin):
+    list_display = ('liturgical_year', 'liturgical_season', 'week_number',
+        'introit', 'collect', 'offertory', 'communion_antiphon', 'postcommunion', 'conclusion', 'background')
+
+class ordinaryTimeReading(admin.ModelAdmin):
+    list_display = ('liturgical_year', 'liturgical_season', 'week_number',
+        'first_reading', 'psalm', 'second_reading', 'gospel_acclamation', 'gospel')
+
+class adventPrayer(admin.ModelAdmin):
+    list_display = ('liturgical_year', 'liturgical_season', 'week_number',
+        'introit', 'collect', 'offertory', 'communion_antiphon', 'postcommunion', 'conclusion', 'background')
+
+class adventReading(admin.ModelAdmin):
+    list_display = ('liturgical_year', 'liturgical_season', 'week_number',
+        'first_reading', 'psalm', 'second_reading', 'gospel_acclamation', 'gospel')
+
+admin.site.register(OrdinaryTimePrayer, ordinaryTimePrayer)
+admin.site.register(OrdinaryTimeReading, ordinaryTimeReading)
+admin.site.register(AdventPrayer, adventPrayer)
+admin.site.register(AdventReading, adventReading)
--- ordinaryform/models.py
+++ ordinaryform/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 # Create your models here.
+
+class OrdinaryTimePrayer(models.Model):
+    liturgical_year = models.CharField(max_length = 128)
+    liturgical_season = models.CharField(max_length = 256)
+    week_number = models.CharField(max_length = 128)
+    introit = HTMLField()
+    collect = HTMLField()
+    offertory = HTMLField()
+    communion_antiphon = HTMLField()
+    postcommunion = HTMLField()
+    conclusion = HTMLField()
+    background = models.CharField(max_length = 512)
+    week_colour = models.CharField(max_length = 128)
+
+class OrdinaryTimeReading(models.Model):
+    liturgical_year = models.CharField(max_length = 128)
+    liturgical_season = models.CharField(max_length = 256)
+    week_number = models.CharField(max_length = 128)
+    week_day = models.CharField(max_length = 128)
+    first_reading = HTMLField()
+    psalm = HTMLField()
+    second_reading =  HTMLField()
+    gospel_acclamation = HTMLField()
+    gospel = HTMLField()
+
+class AdventPrayer(models.Model):
+    liturgical_year = models.CharField(max_length = 128)
+    liturgical_season = models.CharField(max_length = 256)
+    week_number = models.CharField(max_length = 128)
+    introit = HTMLField()
+    collect = HTMLField()
+    offertory = HTMLField()
+    communion_antiphon = HTMLField()
+    postcommunion = HTMLField()
+    conclusion = HTMLField()
+    background = models.CharField(max_length = 512)
+    week_colour = models.CharField(max_length = 128)
+
+class AdventReading(models.Model):
+    liturgical_year = models.CharField(max_length = 128)
+    liturgical_season = models.CharField(max_length = 256)
+    week_number = models.CharField(max_length = 128)
+    week_day = models.CharField(max_length = 128)
+    first_reading = HTMLField()
+    psalm = HTMLField()
+    second_reading =  HTMLField()
+    gospel_acclamation = HTMLField()
+    gospel = HTMLField()
